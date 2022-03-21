423c2cb4538920ad3bb79f0709829c8eb5646419
kriga1 <gaborkr@gmail.com>
2022-03-21 11:22:15 +0100

    Creating

---
+++ src/main/java/hu/unideb/inf/Main.java
@@ -0,0 +1,14 @@
+package hu.unideb.inf;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        MainApp.main(args);
+        System.out.println("Hello World");
+    }
+
+}
