33b60566b621ce21c9e3402f958384f270fb9666
skaralb7 <skaralb7@gmail.com>
2022-03-30 11:21:47 +0200

    changes

---
+++ src/main/main/java/hu/unideb/inf/LogIn.java
@@ -0,0 +1,54 @@
+package hu.unideb.inf;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LogIn implements Initializable {
+
+    @FXML
+    private Button button_login;
+
+    @FXML
+    private TextField tf_username;
+
+    @FXML
+    private TextField tf_password;
+
+    @FXML
+    private Button button_sing_up;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        button_login.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                DBUtils.logInUser(event,tf_username.getText(),tf_password.getText());
+            }
+        });
+
+        button_sing_up.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                DBUtils.changeScene(event, "/fxml/registration.fxml","Sign up!",null);
+                System.out.println("click");
+            }
+        });
+    }
+
+
+    public static void main(String[] args) {
+
+        Main.main(args);
+    }
+
+
+}
